14c27052844b6c42c798b6aed87a1e14eb92d8b6
Hakeem Wikireh <101910136+Hakeempy@users.noreply.github.com>
2023-07-19 13:03:28 +0000

    Update ML_web_app.py

--- ML_web_app.py
+++ ML_web_app.py
@@ -29,7 +29,7 @@ def main():
     
     # model = joblib.load("C:/Users/User/.spyder-py3/model_joblib_gr")
     
-    pkl_path = Path(__file__).parents[1] /'model_joblib_gr'
+    model = Path(__file__).parents[1] /'model_joblib_gr'
     
     p1 = st.slider('Enter your age', 18, 100)
     
